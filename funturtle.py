bc8de0bde7c1528420b02102e1aee44856955534
salwa19-meet <salwa19@meet.mit.edu>
2017-07-31 18:44:22 +0300

    Stopping here

--- funturtle.py
+++ funturtle.py
@@ -1,14 +1,70 @@
 
-UP_ARROW = "UP"
-LEFT_ARROW = "LEFT"
-DOWN_ARROW = "DOWN"
-RIGHT_ARROW = "RIGHT"
-SPACEBAR = "SPACE"
+def up():
+    global direction
+    direction = UP
+    print("you pressed up")
+    old_pos = turtle.pos()
+    x = old_pos[0]
+    y = old_pos[1]
+    turtle.goto(x, y+10)
+    print(turtle.pos())
+def down():
+    global direction
+    direction = DOWN
+    print("you pressed down")
+    old_pos = turtle.pos()
+    x = old_pos[0]
+    y = old_pos[1]
+    turtle.goto(x, y-10)
+    print(turtle.pos())
+def left():
+    global direction
+    direction = LEFT
+    print('you pressed left')
+    old_pos = turtle.pos()
+    x = old_pos[0]
+    y = old_pos[1]
+    turtle.goto(x-10, y)
+    print(turtle.pos())
+def right():
+    global direction
+    direction = RIGHT
+    print('you pressed right')
+    old_pos = turtle.pos()
+    x = old_pos[0]
+    y = old_pos[1]
+    turtle.goto(x+10, y)
+    print(turtle.pos())
+
+###################################
+import turtle
+
+UP_ARROW = "Up"
+LEFT_ARROW = "Left"
+DOWN_ARROW = "Down"
+RIGHT_ARROW = "Right"
+SPACEBAR = "space"
 UP = 0
 LEFT = 1
 DOWN = 2
 RIGHT = 3
 
-
 direction = UP
+
+turtle.onkeypress(up, UP_ARROW)
+turtle.onkeypress(left, LEFT_ARROW)
+turtle.onkeypress(down, DOWN_ARROW)
+turtle.onkeypress(right, RIGHT_ARROW)
+turtle.listen()
+
+
+
+
+
+
+
+
+
+
+turtle.mainloop()
 
